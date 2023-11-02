bac67aadc9159d35d42a311f5751a5c8c1805d19
Hawi Assefa <146360486+Hawi-Assefa@users.noreply.github.com>
2023-11-02 23:44:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 document.addEventListener('DOMContentLoaded', () => \{\
+    const signupForm = document.getElementById('signup-form');\
+\
+    signupForm.addEventListener('submit', (e) => \{\
+        e.preventDefault();\
+        // Add your code to handle form submission here (e.g., send data to the server).\
+        alert('Thank you for signing up! We will notify you when A-Ok! is ready.');\
+    \});\
+\});\
+}
